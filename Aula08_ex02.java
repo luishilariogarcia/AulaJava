a23513feb3c38fd8bb441e4a991dbfedafa482ef
luishilariogarcia <luishilario@gmail.com>
2022-04-13 16:57:10 +0000

    Exercicio 02

---
+++ Aula08_ex02.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Aula08_ex02 {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();// criando um objeto
+        Scanner console = new Scanner(System.in);
+        System.out.print("Digite o seu nome: ");
+        String nome = console.nextLine();
+        p.setnome(nome);
+        System.out.print("Digite a sua data de  aniversário: ");
+        String datanascimento = console.nextLine();
+        p.setdatanascimento(datanascimento);
+        System.out.print("Digite a sua altura: ");
+        float altura = console.nextFloat();
+        p.setaltura(altura);
+        p.exibedados();
+    }
+    
+}
+
---
+++ CaixasdeDialogo.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class CaixasdeDialogo {
+	public static void main(String[] args) {
+		String nome = null;
+		nome = JOptionPane.showInputDialog("Qual é o seu nome?");
+	}
+}
